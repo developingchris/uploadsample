d9968af7c3325501c2abcad26fd5f1988eefd9d6
Chris Chandler <chris@developingchris.com>
2011-03-02 14:14:32 -0500

    added mvc contrib test helper

---
+++ uploadsample.tests/IntegrationTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace uploadsample.tests
+{
+    public class IntegrationTest
+    {
+    }
+}
---
+++ uploadsample.tests/controllers/TestHomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using uploadsample.Controllers;
+
+namespace uploadsample.tests.controllers
+{
+    public class TestHomeController
+    {
+        public TestHomeController() : base()
+        {
+            _controller = new HomeController();
+        }
+
+        private HomeController _controller;
+
+        [Fact]
+        public void Index_Shows_View_With_No_ViewData()
+        {
+            var result = _controller.Index();
+            Assert.NotNull(result);
+            Assert.Empty(_controller.ViewData);
+        }
+    }
+}
